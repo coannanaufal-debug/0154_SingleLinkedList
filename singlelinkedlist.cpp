3f7fb3a3a962ef9d3c6238b3d70883191137e949
coannanaufal-debug <coanna.naufal@gmail.com>
2026-04-24 15:31:01 +0700

    membuat addnode untuk node baru

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -17,5 +17,25 @@ public:
         START = NULL;
     }
 
-    
+    void addNode() {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node* newBaru = new Node();
+        newBaru->noMhs = nim;
+        
+
+        if (START == NULL|| <= Start ->noMhs) {
+            if (START != NULL && START->noMhs == nim)
+             {
+                cout << "\nDuplikasi noMhs tidak diizinkan.\n" << endl;
+                delete newBaru; // Menghapus node yang baru dibuat
+                return;
+            }
+          
+    }
+        Node *previous = Start;
+        Node *current = Start;
+
 };
